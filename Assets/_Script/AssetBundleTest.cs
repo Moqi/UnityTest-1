55cc3483ae4e0451c133278b280261da1801ec65
AmbBAI <amb.main@gmail.com>
2014-08-16 15:20:58 +0800

    assetbundle update

--- Assets/_Script/AssetBundleTest.cs
+++ Assets/_Script/AssetBundleTest.cs
@@ -7,27 +7,21 @@ public class AssetBundleTest : MonoBehaviour {
 	private string localRoot = "";
 	private string bundleExt = "assetbundle";
 
-	delegate IEnumerator AssetBundleLoadDelegate(AssetBundle assetBundle);
 	List<GameObject> objectList = new List<GameObject>();
 
 	int totalTask = 0;
 	int loadTask = 0;
 	float asyncTaskStartTime = 0;
 
-	Dictionary<string, AssetBundle> assetBundleDict = new Dictionary<string, AssetBundle>();
-
+	AssetBundleReader reader = null;
 
 	void Awake () {
-		localRoot =
-#if UNITY_EDITOR
-		"file://" + Application.streamingAssetsPath;
-#elif UNITY_ANDROID
-		Application.streamingAssetsPath;
-#else
-		"file://" + Application.streamingAssetsPath;
-#endif
-
 		Screen.SetResolution(600, 960, true);
+	}
+
+	void Start ()
+	{
+		reader = AssetBundleReader.Instance;
 	}
 
 	void OnGUI ()
@@ -39,23 +33,14 @@ public class AssetBundleTest : MonoBehaviour {
 			LoadResources();
 		} autoY += buttonH;
 
-		if (GUI.Button(new Rect(0, autoY, 200, buttonH), "LoadAssetBundle"))
+		if (GUI.Button(new Rect(0, autoY, 200, buttonH), "LoadAssetsAsync"))
 		{
-			OnAsyncTaskStart();
-			StartCoroutine(LoadABResource("test", loadCallBack1));
-			StartCoroutine(LoadABResource("player", loadCallBack2));
-		} autoY += buttonH;
-
-		if (GUI.Button(new Rect(0, autoY, 200, buttonH), "LoadAssetBundleWithBuffer"))
-		{
-			OnAsyncTaskStart();
-			StartCoroutine(LoadABResourceWithBuffer("test", loadCallBack1));
-			StartCoroutine(LoadABResourceWithBuffer("player", loadCallBack2));
+			LoadAssetsAsync();
 		} autoY += buttonH;
 
 		if (GUI.Button(new Rect(0, autoY, 200, buttonH), "ClearAssetBundleBuffer"))
 		{
-			ResetAssetBundleBuffer();
+			reader.UnloadAllAssetBundle();
 		} autoY += buttonH;
 
 		if (GUI.Button(new Rect(0, autoY, 200, buttonH), "ClearObjects"))
@@ -67,7 +52,7 @@ public class AssetBundleTest : MonoBehaviour {
 		{
 			ClearObjects();
 			Resources.UnloadUnusedAssets();
-			ResetAssetBundleBuffer();
+			reader.UnloadAllAssetBundle();
 		} autoY += buttonH;
 	}
 
@@ -75,62 +60,17 @@ public class AssetBundleTest : MonoBehaviour {
 	{
 		float startTime = Time.realtimeSinceStartup;
 
-		Object preAtomBall = Resources.Load("pre_AtomBall");
+		Object preAtomBall = Resources.Load("test/pre_AtomBall");
 		objectList.Add(Instantiate(preAtomBall) as GameObject);
 
-		Object preSpikeBall = Resources.Load("pre_SpikeBall");
+		Object preSpikeBall = Resources.Load("test/pre_SpikeBall");
 		objectList.Add(Instantiate(preSpikeBall) as GameObject);
 
-		Object prePlayer = Resources.Load("pre_Player");
+		Object prePlayer = Resources.Load("player/pre_Player");
 		objectList.Add(Instantiate(prePlayer) as GameObject);
 
 		float endTime = Time.realtimeSinceStartup;
 		Debug.Log(string.Format("LoadResources Cost: {0}", endTime - startTime));
-	}
-
-	IEnumerator LoadABResource(string package, AssetBundleLoadDelegate loadCallBack)
-	{
-		while (!Caching.ready)
-			yield return null;
-
-		string loadPath = string.Format("{0}/{1}.{2}",
-			localRoot, package, bundleExt);
-		Debug.Log(loadPath);
-
-		using (WWW www = WWW.LoadFromCacheOrDownload(loadPath, 1))
-		{
-			yield return www;
-
-			AssetBundle assetBundle = www.assetBundle;
-
-			yield return StartCoroutine(loadCallBack(assetBundle));
-
-			assetBundle.Unload(false);
-
-			www.Dispose();
-		}
-		yield return null;
-	}
-
-	IEnumerator loadCallBack1(AssetBundle assetBundle)
-	{
-		Object preAtomBall = assetBundle.Load("pre_AtomBall");
-		objectList.Add(Instantiate(preAtomBall) as GameObject);
-		OnLoadDone();
-		yield return null;
-
-		Object preSpikeBall = assetBundle.Load("pre_SpikeBall");
-		objectList.Add(Instantiate(preSpikeBall) as GameObject);
-		OnLoadDone();
-		yield return null;
-	}
-
-	IEnumerator loadCallBack2(AssetBundle assetBundle)
-	{
-		Object prePlayer = assetBundle.Load("pre_Player");
-		objectList.Add(Instantiate(prePlayer) as GameObject);
-		OnLoadDone();
-		yield return null;
 	}
 
 	void OnAsyncTaskStart()
@@ -150,47 +90,29 @@ public class AssetBundleTest : MonoBehaviour {
 		}
 	}
 
-	IEnumerator LoadAssetBundle(string package)
+	void LoadAssetsAsync()
 	{
-		while (!Caching.ready)
-			yield return null;
+		OnAsyncTaskStart();
+		StartCoroutine(reader.LoadAssetAsync("test", "pre_AtomBall", typeof(GameObject),
+			delegate(Object asset)
+			{
+				objectList.Add(Instantiate(asset) as GameObject);
+				OnLoadDone();
+			}));
 
-		string loadPath = string.Format("{0}/{1}.{2}",
-			localRoot, package, bundleExt);
-		Debug.Log(loadPath);
+		StartCoroutine(reader.LoadAssetAsync("test", "pre_SpikeBall", typeof(GameObject),
+			delegate(Object asset)
+			{
+				objectList.Add(Instantiate(asset) as GameObject);
+				OnLoadDone();
+			}));
 
-		using (WWW www = WWW.LoadFromCacheOrDownload(loadPath, 1))
-		{
-			yield return www;
-
-			AssetBundle assetBundle = www.assetBundle;
-
-			assetBundleDict[package] = assetBundle;
-
-			www.Dispose();
-		}
-		yield return null;
-	}
-
-	IEnumerator LoadABResourceWithBuffer(string package, AssetBundleLoadDelegate loadCallBack)
-	{
-		if (!assetBundleDict.ContainsKey(package))
-			yield return StartCoroutine(LoadAssetBundle(package));
-
-		AssetBundle assetBundle = null;
-		assetBundleDict.TryGetValue(package, out assetBundle);
-		if (assetBundle == null) yield break;
-
-		yield return StartCoroutine(loadCallBack(assetBundle));
-	}
-
-	void ResetAssetBundleBuffer()
-	{
-		foreach (KeyValuePair<string, AssetBundle> itor in assetBundleDict)
-		{
-			(itor.Value).Unload(false);
-		}
-		assetBundleDict.Clear();
+		StartCoroutine(reader.LoadAssetAsync("player", "pre_Player", typeof(GameObject),
+			delegate(Object asset)
+			{
+				objectList.Add(Instantiate(asset) as GameObject);
+				OnLoadDone();
+			}));
 	}
 
 	void ClearObjects()
--- Assets/_Script/Editor/ExportAssetBundles.cs
+++ Assets/_Script/Editor/ExportAssetBundles.cs
@@ -1,41 +1,64 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
-public class ExportAssetBundles {
+public class ExportAssetBundles
+{
 
-	// Store current texture format for the TextureProcessor.
-	public static TextureImporterFormat textureFormat;
+    // Store current texture format for the TextureProcessor.
+    //public static TextureImporterFormat textureFormat;
 
-	[MenuItem("Assets/Build AssetBundle From Selection - Android")]
-	static void ExportResourceAndroid () {
-		ExportResource(BuildTarget.Android);		
-	}	
+    public static string packPath = "Assets/Resources/";
+    public static string exportPath = "Assets/StreamingAssets/";
 
-	[MenuItem("Assets/Build AssetBundle From Selection - iOS")]
-	static void ExportResourceIOS () {
-		ExportResource(BuildTarget.iPhone);
-	}
+    static void ExportResource(Object[] objs, string path)
+    {
+        BuildTarget target =
+#if UNITY_ANDROID
+ BuildTarget.Android;
+#elif UNITY_IPHONE
+			BuildTarget.iPhone;
+#else
+			BuildTarget.StandaloneWindows;
+#endif
 
-	static void ExportResource (BuildTarget target) {
-		// Bring up save panel.
-		string path = EditorUtility.SaveFilePanel("Save Resource", "", "New Resource", "assetbundle");
+        BuildAssetBundleOptions options = BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.CompleteAssets;
 
-		if (path.Length != 0) {
-			// Build the resource file from the active selection.
-			Object[] selection = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+        BuildPipeline.BuildAssetBundle(null, objs, path, options, target);
+    }
 
-			foreach (object asset in selection) {
-				string assetPath = AssetDatabase.GetAssetPath((UnityEngine.Object) asset);
-				if (asset is Texture2D) {
-					// Force reimport thru TextureProcessor.
-					AssetDatabase.ImportAsset(assetPath);
-				}
-			}
+    [MenuItem("Assets/Build AssetBundle From Selection", false, 2000)]
+    static void ExportSelectedResource()
+    {
+        // Build the resource file from the active selection.
+        Object[] selection = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
 
-			BuildPipeline.BuildAssetBundle(Selection.activeObject, selection, path,
-				BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.CompleteAssets, target);
-			Selection.objects = selection;
-		}
-	}
+        //foreach (object asset in selection)
+        //{
+        //	string assetPath = AssetDatabase.GetAssetPath((UnityEngine.Object)asset);
+        //	if (asset is Texture2D)
+        //	{
+        //		// Force reimport thru TextureProcessor.
+        //		AssetDatabase.ImportAsset(assetPath);
+        //	}
+        //}
+
+        // Bring up save panel.
+        string path = EditorUtility.SaveFilePanel("Save Resource", exportPath, "New Resource", "assetbundle");
+
+        if (path.Length != 0)
+        {
+            ExportResource(selection, path);
+        }
+        Selection.objects = selection;
+    }
+
+    [MenuItem("Assets/Clear Cache", false, 2002)]
+    static void ClearCache()
+    {
+        Caching.CleanCache();
+    }
+
 }
